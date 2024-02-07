8660ea0e8b3e82b241e8e022bca70ee3e6c18166
mohammad asif <asifumer974@gmail.com>
2024-02-07 13:17:25 +0500

    Q.2 complete

--- Q.2.dart
+++ Q.2.dart
@@ -1,6 +1,12 @@
-
-
   void main() {
+
+    // Q.2.Write a program that prints the Fibonacci sequence up to a given
+// number using a for loop.
+// Example:
+// Input: 10
+// Output: 0 1 1 2 3 5 8
+
+
   int limit = 6;
   List<int> fibSequence = generateFibonacciSequence(limit);
   print("Fibonacci sequence up to $limit is:");
