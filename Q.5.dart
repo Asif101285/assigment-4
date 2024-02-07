ff475131723856b04965e50249892da2d7231135
mohammad asif <asifumer974@gmail.com>
2024-02-07 15:24:22 +0500

    Q.5 complete

--- Q.5.dart
+++ Q.5.dart
@@ -1,4 +1,12 @@
 void main(){
+
+  //Q.5. Write a program that calculates the sum of all the digits in a given
+// number using a while loop.
+// Example:
+// Input: 12345
+// Output: Sum of digits: 15
+
+
   final myListofNumbers = [1, 2, 3, 4, 5, 64, 7];
 
   var sum = 0.0;
