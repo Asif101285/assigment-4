91d6e66a9ea60df2f5062f4548f8ccbcedd3f6c9
mohammad asif <asifumer974@gmail.com>
2024-02-10 16:55:25 +0500

    Q.14 complete

---
+++ Q.14.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main() {
+// 14.Write a program to make a pyramid pattern with numbers increased by 1.
+//    1
+//   2 3
+//  4 5 6
+// 7 8 9 10
+
+  int number = 1;
+  for (int i = 1; i <= 4; i++) {
+    for (int j = 1; j <= 4 - i; j++) {
+      stdout.write(' ');
+    }
+    for (int k = 1; k <= i; k++) {
+      stdout.write("$number ");
+      number++;
+    }
+    print("");
+  }
+}
