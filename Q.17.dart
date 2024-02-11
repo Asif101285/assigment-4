00bca4bbaa807f3faf3260e6cbd9231bab89d30c
mohammad asif <asifumer974@gmail.com>
2024-02-11 14:58:22 +0500

    Q.17 complete

--- Q.17.dart
+++ Q.17.dart
@@ -1,6 +1,14 @@
 import 'dart:io';
 
 void main() {
+
+  // 17.Write a program that asks the user for their email and password. You
+// are given a list of predefined user credentials (email and password
+// combinations). If the entered email and password match any of the
+// credentials in the list, print "User login successful." Otherwise, keep
+// asking for the email and password until the correct credentials are
+// provided.
+
   Map<String, String> userCredentials = {
     'user1@gmail.com': 'password1',
     'user2@gmail.com': 'password2',
