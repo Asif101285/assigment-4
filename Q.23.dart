d57fbb5c723004bc4f203f36e80ac0edd21e52cf
mohammad asif <asifumer974@gmail.com>
2024-02-12 17:25:48 +0500

    Q.23 complete

---
+++ Q.23.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  // Q.23..Implement a code that finds the average of all the negative numbers
+  // in a list using a for loop and if-else condition.
+
+ List<int> numbers = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11];
+  int sum = 0;
+  int count = 0;
+  for (int i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 0) {
+      sum += numbers[i];
+      count++;
+    }
+  }
+  double average = sum / count;
+  print("Average of negative numbers: $average");
+}
